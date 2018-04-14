f5e0438263c03defea546222726bb48762ea2d59
Christian Agustin <networker@christianagustin.com>
2018-04-13 19:48:14 -0600

    se crean los archivos para crear el ambiente de desarrollo del servicio

--- srv-web-api-user/service/create.sh
+++ srv-web-api-user/service/create.sh
@@ -1,4 +1,5 @@
 mkdir dev
 docker build -t devops/user .
-#docker run -d -it --name=user -p 3000:3000 -v $PWD/dev:/srv-web-api-user devops/user
-docker run -it -d -p 3000:3000 --name=user -v $PWD/dev:/srv-web-api-user devops/user
+docker run -it -d -p 3000:3000 --name=user -v $PWD/dev:/api-user devops/user
+git clone https://github.com/coagus/srv-web-api-user.git dev
+docker exec user npm install -d
