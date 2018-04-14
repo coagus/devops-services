9d900cbea87049fad57873cc2d223ad12cdd6ab7
Christian Agustin <networker@christianagustin.com>
2018-04-14 12:16:51 -0600

    se modifica la referncia a api-user

--- srv-web-api-user/service/create.sh
+++ srv-web-api-user/service/create.sh
@@ -1,5 +1,6 @@
-mkdir dev
-docker build -t devops/user .
-docker run -it -d -p 3000:3000 --name=user -v $PWD/dev:/api-user devops/user
-git clone https://github.com/coagus/srv-web-api-user.git dev
-docker exec user npm install -d
+#web-api
+mkdir api-user
+docker build -t devops/api-user .
+docker run -it -d -p 3000:3000 --name=api-user -v $PWD/api-user:/api-user devops/api-user
+git clone https://github.com/coagus/srv-web-api-user.git api-user
+docker exec api-user npm install -d
